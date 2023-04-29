a15a91b40a2f3e322fdf4559fd1354c2c9459219
andrewpavlov <123@>
2023-04-29 19:57:00 +0300

    initial commit

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,13 +1,10 @@
 import csv
-
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.core.paginator import Paginator
 
-
 def index(request):
     return redirect(reverse('bus_stations'))
-
 
 def bus_stations(request):
     page_number = int(request.GET.get("page", 1))
@@ -18,6 +15,6 @@ def bus_stations(request):
         page = paginator.get_page(page_number)
     context = {
         'bus_stations': page,
-        'page': page_number
+        'page': page
     }
     return render(request, 'stations/index.html', context)
