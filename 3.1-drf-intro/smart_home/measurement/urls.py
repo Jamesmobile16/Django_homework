35cc129498717d7c48ae7a3cb1aeeb48a3c4c647
andrewpavlov <123@>
2023-06-05 12:18:24 +0300

    initial commit serializers

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import SensorsView, SensorView, SensorDeleteView
 
 urlpatterns = [
     # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorsView.as_view()),
+    path('sensor/<pk>/', SensorView.as_view()),
+    path('measurements/', SensorView.as_view()),
+    path('sensor_delete/<id>/', SensorDeleteView.as_view())
 ]
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,29 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView, DestroyAPIView
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, MeasurementSerializer, SensorDetailSerializer
+
+class SensorsView(ListCreateAPIView):
+  queryset = Sensor.objects.all()
+  serializer_class = SensorSerializer
+
+class Sensor12View(CreateAPIView):
+  queryset = Sensor.objects.all()
+  serializer_class = SensorSerializer
+
+class SensorView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+    # def get(self, request):
+    #     return Response({Sensor})
+
+class SensorDeleteView(DestroyAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+    lookup_field = 'id'
+
+
+
+
